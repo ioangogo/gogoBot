70e25239389584cf1a1c26d5176a6f396eee6c6a
Ioan Loosley <legit.ioangogo@gmail.com>
2017-12-07 14:53:09 +0000

    I forgot what i changed

--- bot.py
+++ bot.py
@@ -15,7 +15,22 @@ print(os.getcwd())
 
 logging.basicConfig(level=logging.INFO)
 
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (gogoBot; Ubuntu; Linux x86_64; rv:1) Gecko/20100101 gogoBot/1.0'}
+
 config = configparser.ConfigParser()
+
+def ordenal(number):
+    if number % 10 == 1:
+        return "ˢᵗ"
+    elif number % 10 == 2:
+        return "ⁿᵈ"
+    elif number % 10 == 3:
+        return "ʳᵈ"
+    else:
+        return "ᵗʰ"
+
 
 cmdhelp = {}
 cmds= {}
@@ -30,26 +45,10 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name='type !help for commands'))
 
 async def social(message):
-    command = message.content.split(" ")
+    command = message.content.lower()
+    command = command.split(" ")
     msg = ""
-    if len(command) > 1:
-        if(command[1] == "list"):
-            keynum = len(socialmsg)
-            for key, value in socialmsg.items():
-                comma = ""
-                if keynum != 0:
-                    comma = ", "
-                msg += "{0}{1}".format(key,comma)
-        else:
-            msg = socialmsg[command[1]]
-    else:
-        msg = "You can find " + message.server.name + " on\r\n"
-        keynum = len(socialmsg)
-        for s, txt in socialmsg.items():
-            comma = ""
-            if keynum != 0:
-                    comma = ", "
-            msg += "{0}: {1}{2}".format(s, txt, comma)
+    msg = socialmsg[command[0].replace("!","")]
     return msg
 
 async def next(message):
@@ -57,30 +56,29 @@ async def next(message):
 
     time=now.strftime("%Y-%m-%dT%H:%M:%S.0Z")
     url = "https://www.speq.me/api/streamschedule/?format=json&limit=1&start={0}&user={1}".format(time, speqname)
-    resp = requests.get(url=url)
+    resp = requests.get(url=url, headers=headers)
     data = json.loads(resp.text)
-    nexttime= dateutil.parser.parse(data["results"][0]['starttime'])
-
-    td = nexttime - now
-    intime = ""
-    if td.days != 0:
-        intime += "{0}d, ".format(td.days)
-    if td.seconds // 3600 != 0:
-        intime += "{0}h, ".format(td.seconds // 3600)
-    if td.seconds % 3600 // 60 != 0:
-        intime += "{0}m, ".format(td.seconds % 3600 // 60)
-    if td.seconds % 60 != 0:
-        intime += "{0}s".format(td.seconds % 60)
-
     streamtype = data["results"][0]['title']
-    msg = "The next stream is {0}!!! in {1}({2})".format(streamtype, intime, str(nexttime.strftime('aka %H:%M %-dᵗʰ of %b UTC')))
-
-
+    nexttime= dateutil.parser.parse(data["results"][0]['starttime'])
+    if nexttime > now:
+        td = nexttime - now
+        intime = ""
+        if td.days != 0:
+            intime += "{0}d ".format(td.days)
+        if td.seconds // 3600 != 0:
+            intime += "{0}h ".format(td.seconds // 3600)
+        if td.seconds % 3600 // 60 != 0:
+            intime += "{0}m ".format(td.seconds % 3600 // 60)
+        msg = "The next stream is {0}!!! in {1}({2})".format(streamtype, intime, str(nexttime.strftime('aka %H:%M on the %-d{0} of %b UTC'.format(ordenal(nexttime.day)))))
+    else:
+        msg=""
+        la=discord.utils.get(message.server.members, discriminator = 6142)
+        msg="@Loading Artist#6142 Should be streaming {0}".format(streamtype)
     return msg
 
 async def youtube(message):
     query = message.content.replace("!yt ", "")
-    url = "https://www.googleapis.com/youtube/v3/search?q={1}&part=snippet&maxResults=1&key={0}".format(ytkey,query)
+    url = "https://www.googleapis.com/youtube/v3/search?q={1}&part=snippet&maxResults=1&key={0}&type=video".format(ytkey,query)
     resp = requests.get(url=url)
     data = json.loads(resp.text)
     id = data["items"][0]["id"]["videoId"]
@@ -89,22 +87,23 @@ async def youtube(message):
 async def help(message):
     msg = "Hello, this is gogoBot under the name {0}\r\n".format(client.user.name)
     for key, value in cmdhelp.items():
-        msg+="**{0}**: {1}\r\n".format(key, value)
-    msg += "The source, raw source: https://github.com/ioangogo/gogoBot/"
+        msg+="\r\n**{0}**: {1}\r\n".format(key, value)
+    msg += "\r\nThe source, raw source: https://github.com/ioangogo/gogoBot/"
     await client.send_message(message.author, msg)
     return ""
 
 @client.event
 async def on_message(message):
+    command = message.content.lower()
+    print(command)
     if not message.author.bot and message.content.startswith("!"):
-        command = message.content.split(" ")
-        try:
-            await client.send_typing(message.channel)
+        command = command.split(" ")
+        if command[0] in cmds:
+            if not message.content.startswith("!help"):
+                await client.send_typing(message.channel)
             msg = await cmds[command[0]](message)
             if msg != "":
                 tmp = await client.send_message(message.channel, msg)
-        except KeyError:
-            await client.send_message(message.author, "The command you entered is invalid or has not been setup")
 
 @client.event
 async def on_server_join(server):
@@ -120,11 +119,11 @@ if os.path.exists("config.ini"):
         sys.exit("Error, Missing token, please make a bot for this")
     socialmsg={}
     if 'Messages' in config:
-        cmdhelp["!social"] = "This command prints out the urls for the social feeds. Syntax: ```!social [name|list]```"
-        cmds["!social"]=social
         messages = config['Messages']
         for site in messages:
+             cmds["!"+site]=social
              socialmsg[site] = messages[site]
+        cmdhelp["![site]"] = "This command prints out the url for the site that called it. list of sites: {0}".format( list(socialmsg))
     if 'speqname' in config['Config']:
         cmdhelp["!next"] = "This command returns the time remaining till the next stream, all times are in utc"
         cmds["!next"]=next
@@ -138,3 +137,5 @@ else:
     sys.exit("Error, No Config")
 
 client.run(token)
+
+
